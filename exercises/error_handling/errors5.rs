a79b5c96c412ba355c89b751008d1572d55e3f3d
Martina <martina@martina>
2022-10-16 11:02:21 -0500

    Error handling

--- exercises/error_handling/errors5.rs
+++ exercises/error_handling/errors5.rs
@@ -24,6 +24,7 @@ use std::num::ParseIntError;
 fn main() -> Result<(), Box<dyn error::Error>> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
+    
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
     Ok(())
 }
